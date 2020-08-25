2292e26f3a35f55b09d3ca152abea2088065e9e3
Deric Cheung <cheung.deric@gmail.com>
2020-08-25 14:43:49 -0600

    Add comment about LinkedHashMap

--- T4-Symbols/nested/include/MethodSymbol.h
+++ T4-Symbols/nested/include/MethodSymbol.h
@@ -34,7 +34,13 @@
 
 class MethodSymbol : public Symbol, public Scope {
 private:
-    std::vector<std::shared_ptr<Symbol>> orderedArgs;
+    std::vector<std::shared_ptr<Symbol>> orderedArgs; // Note: The original Java implementation used a data structure 
+                                                      // called a LinkedHashMap which as no direct equivalent in C++
+                                                      // Essentially it is a Hash Map implementation that retains the original
+                                                      // insertion ordering of its elements.
+                                                      // For simplicity, this C++ implementation uses a vector and performs
+                                                      // a linear scan to resolve symbols. However, for larger projects this
+                                                      // simple alternative may not be ideal for performance.
     std::shared_ptr<Scope> enclosingScope;
 public:
     MethodSymbol(std::string name, std::shared_ptr<Type> retType, std::shared_ptr<Scope> enclosingScope);
